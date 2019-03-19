150e7aff0b369c0f6e6832cc022aad19745bab75
Robin Wilson <robin@rtwilson.com>
2019-03-19 11:39:31 +0000

    First commit

---
+++ data/ActiveSensors_1.js
@@ -0,0 +1,11 @@
+var json_ActiveSensors_1 = {
+"type": "FeatureCollection",
+"name": "ActiveSensors_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Name": "Sensor 1\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3811606, 50.9247946 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "Name": "Sensor 2\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.381638, 50.9231901 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "Name": "Sensor 3\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3881802, 50.9204951 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "Name": "Sensor 4\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3849428, 50.9256316 ] } }
+]
+}
