598c1b59873be44c72db237d740b91ee6e202bf1
Robin Wilson <robin@rtwilson.com>
2019-03-19 12:24:19 +0000

    Added first go at AURN sensor display

---
+++ data/AURN_Soton_Feb19Stats_2.js
@@ -0,0 +1,9 @@
+var json_AURN_Soton_Feb19Stats_2 = {
+"type": "FeatureCollection",
+"name": "AURN_Soton_Feb19Stats_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "field_1": 0.0, "site_id": "SA33", "site_name": "Southampton A33", "latitude": 50.920265, "longitude": -1.463484, "mean": 25.766164154103841, "max": 95.9, "min": 0.5, "50%": 20.7 }, "geometry": { "type": "Point", "coordinates": [ -1.463484, 50.920265 ] } },
+{ "type": "Feature", "properties": { "field_1": 1.0, "site_id": "SOUT", "site_name": "Southampton Centre", "latitude": 50.90814, "longitude": -1.395778, "mean": 26.940404040404026, "max": 97.4, "min": -5.3, "50%": 21.9 }, "geometry": { "type": "Point", "coordinates": [ -1.395778, 50.90814 ] } }
+]
+}
