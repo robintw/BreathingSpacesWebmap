d933e74f9551e36509fbd432f880692f3d6c09af
Robin Wilson <robin@rtwilson.com>
2019-04-16 21:43:30 +0100

    Added graphs for Breathing Spaces sensors + updated new sensor locs

--- data/ActiveSensors_1.js
+++ data/ActiveSensors_1.js
@@ -1,11 +1,89 @@
 var json_ActiveSensors_1 = {
-"type": "FeatureCollection",
-"name": "ActiveSensors_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "fid": "1", "Name": "Sensor 1\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3811606, 50.9247946 ] } },
-{ "type": "Feature", "properties": { "fid": "2", "Name": "Sensor 2\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.381638, 50.9231901 ] } },
-{ "type": "Feature", "properties": { "fid": "3", "Name": "Sensor 3\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3881802, 50.9204951 ] } },
-{ "type": "Feature", "properties": { "fid": "4", "Name": "Sensor 4\n", "description": "Graph link here" }, "geometry": { "type": "Point", "coordinates": [ -1.3849428, 50.9256316 ] } }
-]
+    "type": "FeatureCollection",
+    "name": " Active Sensors_April19",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "fid": 1,
+                "Name": "Priory Rd (North)",
+                "sensor_id": "nesta-2",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?orgId=1&panelId=3"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.3811606, 50.9247946]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": 2,
+                "Name": "Priory Rd (South)",
+                "sensor_id": "nesta-1",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?orgId=1&panelId=2"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.381638, 50.9231901]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": 3,
+                "Name": "Horsehoe Bridge",
+                "sensor_id": "nesta-4",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?orgId=1&panelId=4"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.3881802, 50.9204951]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": 4,
+                "Name": "Kent Rd",
+                "sensor_id": "nesta-5",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?orgId=1&panelId=5"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.3849428, 50.9256316]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": 5,
+                "Name": "St Denys Rd",
+                "sensor_id": "nesta-7",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?orgId=1&panelId=7"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.3856616, 50.9249299]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "fid": 6,
+                "Name": "Portswood Rd",
+                "sensor_id": "nesta-6",
+                "graph_url": "https:\/\/opennms.computenodes.net\/grafana\/d-solo\/xSYPGSRZk\/st-denys-overview?panelId=6&orgId=1"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-1.3928875, 50.925843]
+            }
+        }
+    ]
 }
