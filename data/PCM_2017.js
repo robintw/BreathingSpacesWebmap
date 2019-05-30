ab281da27cbee895fadf7a0a7026235643cb28a2
Robin Wilson <robin@rtwilson.com>
2019-05-30 10:46:26 +0100

    Added PCM 2016 data and tidied up code

--- data/PCM_2017.js
+++ data/PCM_2017.js
@@ -1,4 +1,4 @@
-var json_PCM = {
+var json_PCM_2017 = {
 "type": "FeatureCollection",
 "name": "PCM_Grid_Clipped_1",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
